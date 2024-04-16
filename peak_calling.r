485dd8e22f78dce490759353fd3596b019b19cd2
Andrea Finocchio <finoccha@tcd.ie>
2024-04-16 16:18:36 +0100

    macs3 finished

--- peak_calling.r
+++ peak_calling.r
@@ -1,12 +1,21 @@
-library(ChIPpeakanno)
+library(ChIPpeakAnno)
 library(dplyr)
 library(tidyr)
+library(reticulate)
+conda_list()
+conda_list()[[1]][5] %>% 
+  use_condaenv(required = TRUE)
+
+macs3_path <- '/Users/frankwellmer/miniconda3/envs/chipseq/bin/macs3'
 
 
+'''Set up where you want the output files to be made, might be 
+easier to set this wherever you have the sample info sheet'''
+setwd('/Users/frankwellmer/joe/chip_pipeline/')
 
 '''Just setting up the little functions that will be used in the script'''
 get_unique_comparisons <- function(data) {
-  unique_values <- unique(data$genotype)
+  unique_values <- unique(data)
   num_values <- length(unique_values)
   
   comparisons <- list()
@@ -21,7 +30,7 @@ get_unique_comparisons <- function(data) {
 }
 
 '''Loading in the sample sheet - ensure the IPs and Controls are loaded as IP and Input, respectively'''
-sample_sheet <- read.csv('~/Downloads/test_chip.csv') %>% mutate(comparison=paste0(Genotype,'_',Condition)
+sample_sheet <- read.csv('./sample_sheet.csv') %>% dplyr::mutate(comparison=paste0(Genotype,'_',Condition)
 )
 
 '''All comparisons within the sample sheet'''
@@ -29,21 +38,44 @@ comps <- get_unique_comparisons(sample_sheet$comparison)
 
 '''macs3 - peak calling and differential peak analysis'''
 for(i in unique(sample_sheet$Genotype)){
-    subset_sample <-  sample_sheet  %>% filter(Genotype==i)
+    subset_sample <-  sample_sheet  %>% dplyr::filter(Genotype==i)
     for(j in unique(subset_sample$Condition)){
-        data <- subset_sample %>% filter(Condition == j)
-        ips <-  data %>% filter(Control=='IP')
-        inputs <- data %>% filter(Control=='Input')
+        data <- subset_sample %>% dplyr::filter(Condition == j)
+        ips <-  data %>% dplyr::filter(Control=='IP')
+        inputs <- data %>% dplyr::filter(Control=='Input')
         ip_info <-  paste(ips$bampath, collapse = " ")
         input_info <- paste(inputs$bampath, collapse = " ")
         outname <- paste0(ips$Genotype[1],'_',ips$Condition[1],'_macs3')
-        macs3_command <- paste0('macs3 callpeak -f BAMPE -t ', ip_info,'-c ',input_info,
-        '--gsize 119485143 -n ', outname)
-        print(macs3_command)
+        macs3_command <- paste0(macs3_path,' callpeak -f BAMPE -t ', ip_info,' -c ',input_info,
+        ' --gsize 119485143 --bdg -n ', outname)
+        system(macs3_command)
     }
 }
 
+macs_peaks <- list.files('./',pattern='bdg')
+macs3 bdgdiff -t1 treatment1.bedGraph -c1 control1.bedGraph -t2 
+treatment2.bedGraph -c2 control2.bedGraph --depth1 1.0 --depth2 1.0 -o output.bedGraph --minlen 500 --maxgap 1000 --cutoff 1.0
 
+
+
+for(i in comps){
+  comp1_IP <- unique(macs_peaks[grepl(i[1], macs_peaks)])
+  comp1_IP <- comp1_IP[grepl('treat', comp1_IP)]
+  comp1_input <- unique(macs_peaks[grepl(i[1], macs_peaks)])
+  comp1_input <- comp1_input[grepl('control', comp1_input)]
+  
+  comp2_IP <- unique(macs_peaks[grepl(i[2], macs_peaks)])
+  comp2_IP <- comp2_IP[grepl('treat', comp2_IP)]
+  comp2_input <- unique(macs_peaks[grepl(i[2], macs_peaks)])
+  comp2_input <- comp2_input[grepl('control', comp2_input)]
+  
+  
+  outname <- paste0(i[1],'_',i[2],'_macs_diffpeaks.bedgraph')
+  
+  sicer_command <- paste0(macs3_path, ' bdgdiff -t1 ', comp1_IP, ' -c1 ', comp1_input,' -t2 ',comp2_IP,' -c2 ',comp2_input,' -s tair10 -o ', outname ,
+                          ' --depth1 1.0 --depth2 1.0 -o output.bedGraph --minlen 500 --maxgap 1000 --cutoff 1.0')
+  print(sicer_command)
+}
 
 
 '''SICER2 - epic2 does not want to run on MAC anymore - mostly use these results as
@@ -55,11 +87,11 @@ test_list <- list()
 
 
 for(i in unique(sample_sheet$Genotype)){
-    subset_sample <-  sample_sheet  %>% filter(Genotype==i)
+    subset_sample <-  sample_sheet  %>% dplyr::filter(Genotype==i)
     for(j in unique(subset_sample$Condition)){
-        data <- subset_sample %>% filter(Condition == j)
-        ips <-  data %>% filter(Control=='IP')
-        inputs <- data %>% filter(Control=='Input')
+        data <- subset_sample %>% dplyr::filter(Condition == j)
+        ips <-  data %>% dplyr::filter(Control=='IP')
+        inputs <- data %>% dplyr::filter(Control=='Input')
         ip_info <-  paste(ips$bampath, collapse = " ")
         input_info <- paste(inputs$bampath, collapse = " ")
         merge_outname_ips <- paste0(ips$Genotype[1],'_',ips$Condition[1],'_', ips$Control[1],'_merged.bam')
@@ -76,11 +108,11 @@ for(i in unique(sample_sheet$Genotype)){
 
 
 for(i in unique(sample_sheet$Genotype)){
-    subset_sample <-  sample_sheet  %>% filter(Genotype==i)
+    subset_sample <-  sample_sheet  %>% dplyr::filter(Genotype==i)
     for(j in unique(subset_sample$Condition)){
-        data <- subset_sample %>% filter(Condition == j)
-        ips <-  data %>% filter(Control=='IP')
-        inputs <- data %>% filter(Control=='Input')
+        data <- subset_sample %>% dplyr::filter(Condition == j)
+        ips <-  data %>% dplyr::filter(Control=='IP')
+        inputs <- data %>% dplyr::filter(Control=='Input')
         ip_info <-  paste(ips$bampath, collapse = " ")
         input_info <- paste(inputs$bampath, collapse = " ")
         merge_outname_ips <- paste0(ips$Genotype[1],'_',ips$Condition[1],'_merged.bam')
